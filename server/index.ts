098d950c22717a7131a63cb805979775dbe4d41c
Tushar Vadodariya <tusharvadodariya11@gmail.com>
2025-12-30 18:26:43 +0530

    changes

--- server/index.ts
+++ server/index.ts
@@ -85,14 +85,13 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  const listenOptions: any = { port, host: "0.0.0.0" };
+  if (process.platform !== "win32") {
+    // reusePort is not supported on some Windows environments
+    listenOptions.reusePort = true;
+  }
+
+  httpServer.listen(listenOptions, () => {
+    log(`serving on port ${port}`);
+  });
 })();
